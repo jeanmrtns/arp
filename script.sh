f74b5a5a8605adce5736122f5dafe7156436dbd4
Jean Martins <jeanmrtns4@gmail.com>
2022-03-24 21:55:46 -0300

    Remove 'incompleto' lines

--- script.sh
+++ script.sh
@@ -2,36 +2,31 @@
 
 clear
 
-# arp -na
-# ip addr
-
-# NETID = IP && NetMask
-
-
 NET=$(ifconfig $i | egrep -o "[0-9]{1,3}\.[0-9]{1,3}\." | tail -3 | head -1)
-tabela=''
+tabela=""
 
 echo
 
-for i in {0..31}; do
-	for j in {0..255}; do
-    echo "ping -c 1 -i 1 ${NET}${j}.${i}"
+for i in {3..3}; do
+	for j in {0..10}; do
+    if [ $i != 0 ] && [ $j != 255 ]
+    then
+      echo "ping -c 1 -i 1 ${NET}${i}.${j}"
         echo
-	  aux=`ping -c 1 -i 1 ${NET}${j}.${i} >/dev/null 2>&1 && arp -na | grep -E "${NET}${i}" | awk '{print $2"\t"$4}'`
-    aux2=$(echo $aux | awk '{ print $2;}')
+      aux=`ping -c 1 -i 1 ${NET}${i}.${j} >/dev/null 2>&1 && arp -na | grep -E "${NET}${i}.${j}\)" | awk '{print $2"\t"$4}'`
+      aux2=`echo "${aux}" | awk '{print $2}'`
 
-    if [[ $(echo $aux2) != "<incompleto>" ]]
-      then
-        tabela="${tabela}
+      if [ "${aux2}" != "<incompleto>" ] ; then
+          tabela="${tabela}
 ${aux}"
+      fi
     fi
   done
 done
 clear
 
-# echo "End. Lógico	End. de Hardware"
 echo -e '\n\033[32;1m == End. Lógico | End. de Hardware = \033[m'
-
 TAB=`echo "${tabela}" | grep -v ^$`
+
 echo "${TAB}"
 echo -e '\n\033[32;1m == Finish = \033[m'
